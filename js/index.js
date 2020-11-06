3cc303bf50e1c5c1ff5a4b79110646367aa636c4
Eduardo Ortega <ortegon000@hootmail.com>
2020-11-05 19:44:25 -0600

    first commit

--- js/index.js
+++ js/index.js
@@ -12,18 +12,14 @@
   // ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
   ZoomMtg.preLoadWasm(); // pre download wasm file to save time.
 
-  var API_KEY = "dKYLD98UQJ-hBsqOKFTQT-7jNqTkXZoOA"; // api
-  // var API_KEY = "2AdoFzHO1pf7S6CCjifqHtl20UhCsGv2zHF0"; // sdk
+  var API_KEY = "2AdoFzHO1pf7S6CCjifqHtl20UhCsGv2zHF0";
 
   /**
    * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
    * The below generateSignature should be done server side as not to expose your api secret in public
    * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
    */
-
-  
-  var API_SECRET = "Me2Pr5GotWY287vXjWXjJ6oBgCW4GAvnUZvw"; // api
-  // var API_SECRET = "Bd88Ick8OdSeKyUiuTZnPAC1GjAnSGZh9Xl2"; // sdk
+  var API_SECRET = "Bd88Ick8OdSeKyUiuTZnPAC1GjAnSGZh9Xl2";
 
   // some help code, remember mn, pwd, lang to cookie, and autofill.
   document.getElementById("display_name").value =
@@ -106,7 +102,6 @@
         apiSecret: API_SECRET,
         role: meetingConfig.role,
         success: function (res) {
-          console.log(res);
           console.log(res.result);
           meetingConfig.signature = res.result;
           meetingConfig.apiKey = API_KEY;
@@ -130,7 +125,6 @@
       apiSecret: API_SECRET,
       role: meetingConfig.role,
       success: function (res) {
-        console.log('res');
         console.log(res.result);
         meetingConfig.signature = res.result;
         meetingConfig.apiKey = API_KEY;
